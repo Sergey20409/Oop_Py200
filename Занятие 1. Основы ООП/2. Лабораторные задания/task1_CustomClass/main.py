bc5becd9f4f829eb4166e63969aa260a0a635854
trakt <traktorsergey@gmail.com>
2025-02-06 21:14:19 +0300

    06.02.2025

--- Занятие 1. Основы ООП/2. Лабораторные задания/task1_CustomClass/main.py
+++ Занятие 1. Основы ООП/2. Лабораторные задания/task1_CustomClass/main.py
@@ -1,29 +1,33 @@
 # TODO Написать 3 класса с документацией и аннотацией типов
 from typing import Union
+
 class Tanker:
-    def __init__(self, capacity_volume: float):
+    def __init__(self, capacity_volume: float, crew: int, occupied_volume: int):
 
         self.capacity_volume = capacity_volume
-
-
+        self.crew = crew
+        self.occupied_volume = occupied_volume
 
 
 if __name__ == "__main__":
     # TODO работоспособность экземпляров класса проверить с помощью doctest
     pass
+
 class Bulker:
-    def __init__(self, capacity_weight: float):
+    def __init__(self, capacity_weight: float, crane:int, occupied_weight: int):
 
         self.capacity_weight = capacity_weight
-
-
+        self.crane = crane
+        self.occupied_weight = occupied_weight
 
 if __name__ == "__main__":
         # TODO работоспособность экземпляров класса проверить с помощью doctest
         pass
 
 
-class Tug:
-    def __init__(self, hook_force: float):
+class Tug_Supply:
+    def __init__(self, hook_force: float, capacity_container, occupied_container):
 
         self.hook_force = hook_force
+        self.capacity_container = capacity_container
+        self.occupied_container = occupied_container
